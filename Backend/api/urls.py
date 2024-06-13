ee0c47962012018df184bb5ab7f54f5e1a8efc22
Ahmad <afd08@mail.aub.edu>
2024-06-13 13:12:11 +0300

    users can now view metadata by inserting start and end dates if those values were not given the database provides meta data starting 2 weeks ago from today

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import DocGeneralInfoView
+from .views import view_docs, add_doc
 from . import views
 
 urlpatterns = [
-    path('docs-info', DocGeneralInfoView.as_view()), #redirects to api/views.py
+    path('view-docs', views.view_docs, name = 'view_docs'), #redirects to api/views.py
     path('add-doc', views.add_doc, name='add_doc'),
 ]
--- Backend/api/views.py
+++ Backend/api/views.py
@@ -5,11 +5,31 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import DocGeneralInfo
 from .serializers import DocGeneralInfoSerializer
+from datetime import datetime, timedelta
+
 # Create your views (endpoints) here.
 
-class DocGeneralInfoView(generics.ListAPIView):
-    queryset = DocGeneralInfo.objects.all()
-    serializer_class = DocGeneralInfoSerializer
+@api_view(['GET'])
+def view_docs(request):
+    try:
+        start_date = request.GET.get('start_date')
+        end_date = request.GET.get('end_date')
+
+        if not start_date:
+            start_date = datetime.now() - timedelta(weeks=2)
+        else:
+            start_date = datetime.strptime(start_date, '%Y-%m-%d')
+
+        if not end_date:
+            end_date = datetime.now()
+        else:
+            end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+        docs = DocGeneralInfo.objects.filter(date_submitted__gte=start_date, date_submitted__lt=end_date)
+        serializer = DocGeneralInfoSerializer(docs, many=True)
+        return Response(serializer.data)
+    except Exception as e:
+        return Response({'error': str(e)}, status=400)
 
 @api_view(['POST'])
 def add_doc(request):
