079685ce041c05284aaf9af51e51fdb6e7d96524
Luidovick <luidoviczgheib22@gmail.com>
2024-06-18 22:53:24 +0300

    Logistic Regression Implementation + Test Cases

---
+++ IDP-025/Logistic_Regression.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from pathlib import Path
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, f1_score
+import joblib
+
+current_dir = Path(__file__).resolve().parent
+dataset_path = current_dir.parent / 'IDP-024' / 'Non_legal_dataset.csv'
+
+if not dataset_path.is_file():
+    print(f"File not found: {dataset_path}")
+else:
+    dataset = pd.read_csv(dataset_path)
+
+#print(dataset.head())
+
+vectorizer = TfidfVectorizer(stop_words='english', max_features=1000)
+X = vectorizer.fit_transform(dataset['Text'])
+y = dataset['Label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the Logistic Regression Model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Evaluate the Model
+y_pred = model.predict(X_test)
+
+
+f1 = f1_score(y_test, y_pred, average='weighted')
+print("Weighted F1 Score:", f1)
+
+
+print("Classification Report:")
+print(classification_report(y_test, y_pred, target_names=['Politics','Sport','Technology','Entertainment','Business']))
+
+# Saving the model & Vectorizer
+joblib.dump(model, 'logistic_regression_model.pkl')
+joblib.dump(vectorizer, 'tfidf_vectorizer.pkl')
+
