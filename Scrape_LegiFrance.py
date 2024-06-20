6aeab8527493477ca7e183679fdcc8742082b2f8
Hadi Al Hassan <hadialhassan90@gmail.com>
2024-06-20 10:38:42 +0300

    Clean Code + Error Handling

--- Scrape_LegiFrance.py
+++ Scrape_LegiFrance.py
@@ -1,52 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
-import selenium
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 import pyperclip  # Library to access the clipboard
 import time  # To handle any necessary delays
-from googletrans import Translator
 
-
-def Scrape_LegiFrance(url):    
+def setup_driver():
     driver = webdriver.Chrome()
+    return driver
+
+#save into TXT file format
+def save_content(title,content):
+    filename = f"{title}.txt".replace(" ", "_").replace(":", "").replace("/", "_").replace("\n"," ")
+
+    try:
+        with open(filename, "w", encoding='utf-8') as file:
+            file.write(content)
+
+    except IOError as e:
+        print(f"Error saving content to {filename}: {e}")
+
+def scrape_legifrance(url):
+    """Scrapes content from the given LegiFrance URL and saves it to a text file."""
+    driver = setup_driver()
     try:
         driver.get(url)
-
         wait = WebDriverWait(driver, 10)
-        
+
+        # Wait until the copy button is visible and click it
         copy_button = wait.until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[1]/div/main/div/div/div[1]/div/div/ul/li[2]/button")))
-        
         copy_button.click()
-        
-       #wait for the text to be copied
-        time.sleep(1)  
 
+        # Wait for the text to be copied to the clipboard
+        time.sleep(1)
         copied_text = pyperclip.paste()
-        
-        title = driver.find_element(By.XPATH,"/html/body/div[1]/div/main/div/div/div[2]/h1").text
-        title = title.replace(" ", "_").replace(":", "").replace("/", "_")
 
-        with open(f"{title}.txt", "w", encoding="utf-8") as file:
-            file.write(copied_text)
+        # Get the title of the document
+        title = driver.find_element(By.XPATH, "/html/body/div[1]/div/main/div/div/div[2]/h1").text
         
-        print(f"Text copied and saved to {title}.txt")
+        save_content(title, copied_text)
 
     except Exception as e:
-        print(f"An error occured: {e}")
+        print(f"An error occurred: {e}")
 
     finally:
         driver.quit()
 
 if __name__ == "__main__":
     url = "https://www.legifrance.gouv.fr/cons/id/CONSTEXT000049631354?init=true&page=1&query=&searchField=ALL&tab_selection=constit"
-    Scrape_LegiFrance(url)
-
-    
+    scrape_legifrance(url)
