15f8208bbdeaee352c0e1b93c4095d821484f264
Hadi Al Hassan <hadialhassan90@gmail.com>
2024-06-26 08:39:17 +0300

    Started building llama parse API

---
+++ testllamaparse.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the API key from environment variable
+api_key = os.getenv('LLAMA_CLOUD_API_KEY')
+
+# Bring in dependencies
+from llama_parse import LlamaParse
+from llama_index.core import SimpleDirectoryReader
+
+# Set up parser with the API key
+parser = LlamaParse(
+    result_type="markdown",  # "markdown" and "text" are available
+    api_key=api_key
+)
+
+# Use SimpleDirectoryReader to parse our file
+file_extractor = {".txt": parser}
+documents = SimpleDirectoryReader(input_files=['GRAHAM_v._STATE_OF_WEST_VIRGINIA..txt'], file_extractor=file_extractor).load_data()
+
+# Print the parsed documents
+print(documents)
