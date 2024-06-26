cfb6db86330d8195cfa1db1899ba387ffc287af5
Hadi Al Hassan <hadialhassan90@gmail.com>
2024-06-26 12:24:59 +0300

    Prototype ChatBot implementation, to be used an API later

--- testcohere.py
+++ testcohere.py
@@ -1,20 +1,29 @@
 import cohere 
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 co = cohere.Client(
-  api_key="5KI2hpHsWl3Zz8LloXDBkeHSoPdduH1j50SvCneU", # This is your trial API key
+  api_key=os.getenv("COHERE_API_KEY"), # This is your trial API key
 ) 
 
-query = input()
+while (query := input()) != "f101":
 
-stream = co.chat_stream( 
-  model='command-r-plus',
-  message='<YOUR MESSAGE HERE>',
-  temperature=0.3,
-  chat_history=[],
-  prompt_truncation='AUTO',
-  connectors=[{"id":"web-search"}]
-) 
+  stream = co.chat_stream( 
+    model='command-r-plus',
+    preamble="You are a legal aid Generative AI tool for an intelligent document management system.\
+      Our clients will ask you questions regarding the law, try to answer to the best of your knowledge.\
+      Do not answer non legal questions if the client does not establish relevancy. ",
+    message= f'<Question: {query}>',
+    temperature=0.3,
+    chat_history=[],
+    prompt_truncation='AUTO',
+    connectors=[{"id":"web-search"}]
+  ) 
 
-for event in stream:
-  if event.event_type == "text-generation":
-    print(event.text, end='')
+  for event in stream:
+    if event.event_type == "text-generation":
+      print(event.text, end='')
+
+  print()
