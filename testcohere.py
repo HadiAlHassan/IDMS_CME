77ce03b375cb734b82c3e40539d374ef6aa65bf7
Hadi Al Hassan <hadialhassan90@gmail.com>
2024-06-27 15:12:16 +0300

    altered temperature settings

--- testcohere.py
+++ testcohere.py
@@ -8,15 +8,17 @@ co = cohere.Client(
   api_key=os.getenv("COHERE_API_KEY"), # This is your trial API key
 ) 
 
+preamble = "You are a legal aid Generative AI tool for an intelligent document management system.\
+      Our clients will ask you questions regarding the law, try to answer to the best of your knowledge.\
+      Do not answer non legal questions if the client does not establish relevancy. "
+
 while (query := input()) != "f101":
 
   stream = co.chat_stream( 
     model='command-r-plus',
-    preamble="You are a legal aid Generative AI tool for an intelligent document management system.\
-      Our clients will ask you questions regarding the law, try to answer to the best of your knowledge.\
-      Do not answer non legal questions if the client does not establish relevancy. ",
-    message= f'<Question: {query}>',
-    temperature=0.3,
+    preamble=preamble,
+    message= f'{preamble} <Question: {query}>',
+    temperature=0.5,
     chat_history=[],
     prompt_truncation='AUTO',
     connectors=[{"id":"web-search"}]
