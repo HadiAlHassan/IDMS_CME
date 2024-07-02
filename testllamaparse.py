c6bd74c696013364f739c7d26473e927f9059877
Hadi Al Hassan <hadialhassan90@gmail.com>
2024-07-02 15:28:58 +0300

    Working RAG Proof of Concept

--- testllamaparse.py
+++ testllamaparse.py
@@ -1,9 +1,14 @@
 import os
 from dotenv import load_dotenv
 from llama_parse import LlamaParse
+from llama_index.llms.cohere import Cohere
 from llama_index.core import SimpleDirectoryReader
-from llama_index.core import Document
+from llama_index.core import Settings
+from llama_index.core import VectorStoreIndex
 from llama_index.embeddings.cohere.base import CohereEmbedding
+from llama_index.postprocessor.cohere_rerank import CohereRerank
+from llama_index.core.indices.prompt_helper import PromptHelper
+
 load_dotenv()
 
 llama_cloud_api_key = os.getenv("LLAMA_CLOUD_API_KEY")
@@ -18,10 +23,47 @@ parser = LlamaParse(
 # use SimpleDirectoryReader to parse our file
 file_extractor = {".txt": parser}
 documents = SimpleDirectoryReader(input_files=['GRAHAM_v._STATE_OF_WEST_VIRGINIA..txt'], file_extractor = file_extractor).load_data()
-print(documents[0].text)
 
-"""
-embeder = CohereEmbedding(api_key = cohere_api_key)
+embed_model = CohereEmbedding(
+    api_key = cohere_api_key,
+    model_name="embed-english-v3.0",
+    input_type="search_document",
+    embedding_type="binary",
+)
 
-document_embedding = embeder.get_text_embedding(documents[0].text)
-"""
+index = VectorStoreIndex.from_documents(
+    documents=documents, embed_model=embed_model
+)
+
+embed_model = CohereEmbedding(
+    api_key=cohere_api_key,
+    model_name="embed-english-v3.0",
+    input_type="search_query",
+    embedding_type="float",
+)
+
+Settings.llm = Cohere(api_key = cohere_api_key, model="command")
+Settings.embed_model = embed_model
+
+
+prompt_helper = PromptHelper(
+    context_window=2048,  # set this to the model's maximum context size
+    num_output=512,  # set this to the desired output size
+    chunk_overlap_ratio=0.1,  # set this based on your preference
+    chunk_size_limit=512  # set this based on your preference
+)
+
+Settings.prompt_helper = prompt_helper
+
+# Create a cohere reranker
+cohere_rerank = CohereRerank(api_key= cohere_api_key)
+
+
+
+# Create the query engine
+query_engine = index.as_query_engine(node_postprocessors=[cohere_rerank])
+
+# Generate the response
+response = query_engine.query("explain the case to me",)
+
+print(response)
