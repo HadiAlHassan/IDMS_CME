e2a3a376fbffd046ba6ec60c2ebb55b1964049e0
Hadi Al Hassan <hadialhassan90@gmail.com>
2024-07-02 10:44:38 +0300

    figuring out llamaparse

--- testllamaparse.py
+++ testllamaparse.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from llama_parse import LlamaParse
 from llama_index.core import SimpleDirectoryReader
+from llama_index.core import Document
 from llama_index.embeddings.cohere.base import CohereEmbedding
 load_dotenv()
 
@@ -17,6 +18,10 @@ parser = LlamaParse(
 # use SimpleDirectoryReader to parse our file
 file_extractor = {".txt": parser}
 documents = SimpleDirectoryReader(input_files=['GRAHAM_v._STATE_OF_WEST_VIRGINIA..txt'], file_extractor = file_extractor).load_data()
-print(documents)
+print(documents[0].text)
 
-embedding = CohereEmbedding(api_key= cohere_api_key)
+"""
+embeder = CohereEmbedding(api_key = cohere_api_key)
+
+document_embedding = embeder.get_text_embedding(documents[0].text)
+"""
