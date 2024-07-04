99443cf45dc041e055f0c0ffda62360fa3fbe5d5
Hadi Al Hassan <hadialhassan90@gmail.com>
2024-07-04 12:01:48 +0300

    Updated files

--- testllamaparse.py
+++ testllamaparse.py
@@ -9,6 +9,8 @@ from llama_index.core import VectorStoreIndex
 from llama_index.embeddings.cohere.base import CohereEmbedding
 from llama_index.postprocessor.cohere_rerank import CohereRerank
 from llama_index.core.indices.prompt_helper import PromptHelper
+from llama_index.core.node_parser import HierarchicalNodeParser
+from llama_index.core.node_parser import SentenceSplitter
 
 
 load_dotenv()
@@ -24,45 +26,50 @@ parser = LlamaParse(
     api_key = llama_cloud_api_key,
     parsing_instruction = parsing_prompt
 )
- 
+
 # use SimpleDirectoryReader to parse our file
 file_extractor = {".txt": parser}
 
 parse_begin = time.time()
-documents = SimpleDirectoryReader(input_files=['BELL_ATLANTIC_CORPORATION_et_al.,_Petitioners,__v.__William_TWOMBLY_et_al..txt'], file_extractor = file_extractor).load_data()
+
+documents = SimpleDirectoryReader(input_files=['GRAHAM_v._STATE_OF_WEST_VIRGINIA..txt'],
+                                  file_extractor = file_extractor).load_data()
+
 parse_duration = time.time() - parse_begin
 
 #Setting the embedding and feeding to the index
 embed_model = CohereEmbedding(
     api_key = cohere_api_key,
-    model_name="embed-english-v3.0",
-    input_type="search_document",
-    embedding_type="binary",
+    model_name = "embed-english-v3.0",
+    input_type = "search_document",
+    embedding_type = "int8",
 )
 
 index = VectorStoreIndex.from_documents(
-    documents=documents, embed_model=embed_model
+    documents = documents,
+    embed_model = embed_model,
+    transformations = [SentenceSplitter(chunk_size=1024, chunk_overlap=20)],
 )
 
-embed_model = CohereEmbedding(
-    api_key=cohere_api_key,
-    model_name="embed-english-v3.0",
-    input_type="search_query",
-    embedding_type="float",
-)
+# embed_model = CohereEmbedding(
+#     api_key = cohere_api_key,
+#     model_name = "embed-english-v3.0",
+#     input_type = "search_query",
+#     embedding_type = "float",
+# )
 
 #Chunk size
 Settings.llm = Cohere(api_key = cohere_api_key, model="command-r")
 Settings.embed_model = embed_model
 
-prompt_helper = PromptHelper(
-    context_window=2048,  # set this to the model's maximum context size
-    num_output=512,  # set this to the desired output size
-    chunk_overlap_ratio=0.1,  # set this based on your preference
-    chunk_size_limit=512  # set this based on your preference
- )
+# prompt_helper = PromptHelper(
+#     context_window=2048,  # set this to the model's maximum context size
+#     num_output=512,  # set this to the desired output size
+#     chunk_overlap_ratio=0.1,  # set this based on your preference
+#     chunk_size_limit=512  # set this based on your preference
+#  )
 
-Settings.prompt_helper = prompt_helper
+# Settings.prompt_helper = prompt_helper
 
 # Create a cohere reranker
 cohere_rerank = CohereRerank(api_key= cohere_api_key)
@@ -71,16 +78,14 @@ cohere_rerank = CohereRerank(api_key= cohere_api_key)
 query_engine = index.as_query_engine(node_postprocessors=[cohere_rerank])
 
 
-
-
 # Generate the response
 start_time = time.time()
-response = query_engine.query("Explain to me the details of the case",)
+response = query_engine.query("what were all the names the defendant went by from the case of graham v west virginia",)
 response_time = time.time() - start_time
 
 print(response)
 
-print(f"Parse duration: {parse_duration}\n \
-        Query duration: {response_time}")
+print(f"Parse duration: {parse_duration}\n\
+Query duration: {response_time}")
 
 
