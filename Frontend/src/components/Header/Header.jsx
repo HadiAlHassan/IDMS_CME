a5fac8462ebfe903b407c7ad9b6b72f04a9f935e
Ahmad <afd08@mail.aub.edu>
2024-07-08 16:34:07 +0300

    added a side bar and moved the logout button

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import classes from "./Header.module.css";
 import LogoutButton from "../LogOutButton/LogoutButton";
+import NavBar from "../NavBar/NavBar";
 import SideBarDrawer from "../SideBar/SideBarDrawer";
 function Header() {
   return (
     <div className={classes.Header}>
-      <LogoutButton />
+      <NavBar />
       <p className={classes.TitleWebsite}>Intelligent Data Management System</p>
       <SideBarDrawer />
     </div>
---
+++ Frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import classes from "./NavBar.module.css";
+import LogoutButton from "../LogOutButton/LogoutButton";
+
+export default function NavBar() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <LogoutButton />
+    </Box>
+  );
+
+  return (
+    <div>
+      <IconButton
+        className={classes.NavBarButton}
+        variant="outlined"
+        color="inherit"
+        onClick={toggleDrawer(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
